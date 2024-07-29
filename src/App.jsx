fdbbbd33637fd4a18908db737dd906687d5fd057
Dhurmil0413 <dhrumilvaghasiya413@gmail.com>
2024-07-29 15:47:34 -0600

    The commit to adding new components like Album, and Featured Songs

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import {Player} from './components/Player'
 import {Sidebar} from './components/Sidebar'
+import Display from './components/Display'
 
 function App() {
   return (
     <div className="h-screen w-screen bg-black">
       <div className="h-[90%] flex">
         <Sidebar />
+        <Display/>
       </div>
       <Player/>
     </div>
